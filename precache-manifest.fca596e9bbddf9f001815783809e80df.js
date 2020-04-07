591bd54648b8a078cd3b28ffb621462ebaa5b69c
Uktu94 <utkukaraaslan94@gmail.com>
2020-04-07 22:22:11 +0300

    Updates

---
+++ precache-manifest.fca596e9bbddf9f001815783809e80df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83df4fa149091e7a6876745fc0a31ed6",
+    "url": "/monstersrolodex/index.html"
+  },
+  {
+    "revision": "125680189829db6a8061",
+    "url": "/monstersrolodex/static/css/main.728e6ae8.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monstersrolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monstersrolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "125680189829db6a8061",
+    "url": "/monstersrolodex/static/js/main.cd7568b7.chunk.js"
+  },
+  {
+    "revision": "96f0135039460d4bf74f",
+    "url": "/monstersrolodex/static/js/runtime-main.4a497939.js"
+  }
+]);
